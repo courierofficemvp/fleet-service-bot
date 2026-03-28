b299be00ae60bf409f056d8fa0c61f9f15aca734
Valentyn Maslo <vamas@macbook-air-valentyn-1.home>
2026-03-28 18:35:24 +0100

    fix bot polling and routers

--- bot.py
+++ bot.py
@@ -1,17 +1,27 @@
 import asyncio
+import logging
+
 from aiogram import Bot, Dispatcher
+
 from config import BOT_TOKEN
 from handlers import start, admin, mechanic, accountant
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-
-dp.include_router(start.router)
-dp.include_router(admin.router)
-dp.include_router(mechanic.router)
-dp.include_router(accountant.router)
+logging.basicConfig(level=logging.INFO)
 
 async def main():
+    print("BOT STARTED 🚀")
+
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    # 🔥 ПОДКЛЮЧАЕМ ВСЕ ROUTER
+    dp.include_router(start.router)
+    dp.include_router(admin.router)
+    dp.include_router(mechanic.router)
+    dp.include_router(accountant.router)
+
+    print("POLLING STARTED 🔥")
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
